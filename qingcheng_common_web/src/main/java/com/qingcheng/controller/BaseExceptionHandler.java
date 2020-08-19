2f6e97ce61982ec8e3fc111b10c07b783a21015e
yjlhz <2386987985@qq.com>
2020-08-19 20:11:47 +0800

    加入公共异常处理

---
+++ qingcheng_common_web/src/main/java/com/qingcheng/controller/BaseExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.qingcheng.controller;
+
+import com.qingcheng.entity.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        //打印错误
+        e.printStackTrace();
+        System.out.println("调用异常处理!");
+        //返回异常信息
+        return new Result(1,e.getMessage());
+    }
+}
