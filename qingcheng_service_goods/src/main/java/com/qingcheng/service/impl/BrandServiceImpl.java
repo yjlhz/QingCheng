8d12308c11c4496af2cad31a609536023cfd8a60
yjlhz <2386987985@qq.com>
2020-08-19 17:41:17 +0800

    实现部分增删改查功能

--- qingcheng_service_goods/src/main/java/com/qingcheng/service/impl/BrandServiceImpl.java
+++ qingcheng_service_goods/src/main/java/com/qingcheng/service/impl/BrandServiceImpl.java
@@ -8,8 +8,10 @@ import com.qingcheng.entity.PageResult;
 import com.qingcheng.pojo.goods.Brand;
 import com.qingcheng.service.goods.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class BrandServiceImpl implements BrandService {
@@ -28,4 +30,45 @@ public class BrandServiceImpl implements BrandService {
         Page<Brand> pageResult = (Page<Brand>) brandMapper.selectAll();
         return new PageResult<>(pageResult.getTotal(),pageResult.getResult());
     }
+
+    @Override
+    public List<Brand> findList(Map<String, Object> searchMap) {
+        Example example = creatExample(searchMap);
+        return brandMapper.selectByExample(example);
+
+    }
+
+    @Override
+    public PageResult<Brand> findPage(Map<String, Object> searchMap, int page, int size) {
+        PageHelper.startPage(page, size);
+        Example example = creatExample(searchMap);
+        Page<Brand> pageResult = (Page<Brand>) brandMapper.selectByExample(example);
+        return new PageResult<>(pageResult.getTotal(),pageResult.getResult());
+    }
+
+    @Override
+    public Brand findById(Integer id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void add(Brand brand) {
+        brandMapper.insert(brand);
+    }
+
+    private Example creatExample(Map<String, Object> searchMap){
+        Example example = new Example(Brand.class);
+        Example.Criteria criteria = example.createCriteria();
+        //判断前台传来的条件封装对象不为空，为空则不需要做模糊查询
+        if (searchMap != null){
+            if (searchMap.get("name") != null && !"".equals(searchMap.get("name"))){
+                criteria.andLike("name","%"+(String) searchMap.get("name")+"%");
+            }
+            if (searchMap.get("letter") != null && !"".equals(searchMap.get("letter"))){
+                criteria.andEqualTo("letter",(String)searchMap.get("letter"));
+            }
+        }
+        return example;
+    }
+
 }
--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/BrandController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/BrandController.java
@@ -2,14 +2,13 @@ package com.qingcheng.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.qingcheng.entity.PageResult;
+import com.qingcheng.entity.Result;
 import com.qingcheng.pojo.goods.Brand;
 import com.qingcheng.service.goods.BrandService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/brand")
@@ -26,6 +25,27 @@ public class BrandController {
     @GetMapping("/findPage")
     public PageResult<Brand> findPage(int page,int size){
         return brandService.findPage(page,size);
+    }
+
+    @PostMapping("/findList")
+    public List<Brand> findList(@RequestBody Map searchMap){
+        return brandService.findList(searchMap);
+    }
+
+    @PostMapping("/findPage")
+    public PageResult<Brand> findPage(@RequestBody Map searchMap,int page,int size){
+        return brandService.findPage(searchMap,page,size);
+    }
+
+    @GetMapping("/findById")
+    public Brand findById(Integer id){
+        return brandService.findById(id);
+    }
+
+    @PostMapping("/add")
+    public Result add(@RequestBody Brand brand){
+        brandService.add(brand);
+        return new Result();
     }
 
 }
