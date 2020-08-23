d3641b1299c0027d0a5b9c3c597fd9aedc094ca9
yjlhz <2386987985@qq.com>
2020-08-24 00:56:05 +0800

    重构了上传部分的代码

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
@@ -40,20 +40,17 @@ public class UploadController {
 
     }
 
+    @Autowired
+    private OSS ossClient;
+
 
     @PostMapping("/oss")
     public String ossUpload(@RequestParam("file") MultipartFile file,String folder){
-        // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "oss-cn-beijing.aliyuncs.com";
-        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI4G4qHyJAEvrSZrjRdLBC";
-        String accessKeySecret = "GfxoxC04UxMXInc85CFzG8gVRknsp4";
+
         //oss的仓库名称
         String bucketName = "yjlhz-qingchengplus";
         //保证上传文件名的唯一性
         String fileName = folder+"/"+UUID.randomUUID()+file.getOriginalFilename();
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         try {
             //上传图片
             ossClient.putObject(bucketName,fileName,file.getInputStream());
